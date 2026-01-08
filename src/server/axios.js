06643a6c6b79f9267722761e4f1de49c3acb17bc
IT-HARISH-R <harishmass27.8@gmail.com>
2026-01-08 11:59:53 +0530

    Change api Key

--- src/server/axios.js
+++ src/server/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://astro-backend-1-fftg.onrender.com/",
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://astro-backend-1-fftg.onrender.com/",
+  // baseURL: "http://127.0.0.1:8000/",
   withCredentials: true,
 });
 
